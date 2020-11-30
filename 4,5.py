b5e9b68d540ea597ca8444b9d84cba6ef21bb94a
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2020-11-30 10:12:25 +0100

    Create 4,5.py

---
+++ 4,5.py
@@ -0,0 +1,12 @@
+mylist = [ [i, i, i] for i in range(5)]
+
+
+x = len(mylist)
+
+
+for x in mylist:
+    print(x)
+
+
+# 1d arrays store lists 
+# 2d arrays store coordinates on a cartesian coordinate system
